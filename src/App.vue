78e853b874e2769943cd8dde0062b68ac32b9175
sajibcse68 <sajibcse68@gmail.com>
2018-05-15 16:14:47 +0600

    Add CompletedTodo, GetTodo, CurrentTodos componenets. Add Vuex store & get add/remove/edit working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <CompletedTodos></CompletedTodos>
+    <GetTodo></GetTodo>
+    <CurrentTodos></CurrentTodos>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GetTodo from './components/GetTodo.vue';
+import CurrentTodos from './components/CurrentTodos.vue';
+import CompletedTodos from './components/CompletedTodos.vue';
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    GetTodo,
+    CurrentTodos,
+    CompletedTodos
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: Helvetica, sans-serif;
 }
 </style>
---
+++ src/components/CompletedTodos.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="completed-todos">
+      <h3 v-if="completed.length > 0">
+          <ul class="list-group">
+              <li class="list-group-item" v-for="todo in completed" :key=todo.id>
+                {{todo.body}}
+                <button type="button" 
+                  @click="remove(todo)"
+                  class="btn btn-default btn-sm"
+                  >
+                  <span class="glyphicon glyphicon-remove-circle"></span>
+                  Remove
+                  </button>
+              </li>
+          </ul>
+      </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    remove(todo) {
+      this.$store.dispatch('removeTodo', todo);
+    }
+  },
+  computed: {
+    completed() {
+      return this.$store.getters.completedTodos;
+    }
+  }
+}
+</script>
